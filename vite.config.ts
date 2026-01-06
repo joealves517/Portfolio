1829849a0858e1e48c65c732c03bf48fe9edf892
Oscar Alves <alvesoscar517@gmail.com>
2026-01-06 16:03:55 +0700

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',
+  base: '/Portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
